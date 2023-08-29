bc30f422b0d04bbf41ff83e0d9fe0c42d5bcd456
Yan <yan.doshchinskiy@gmail.com>
2023-08-29 20:39:20 +0600

    feat: header added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,56 @@
 <template>
   <div class="default-layout">
-    <slot />
+    <header class="header">
+      <nav class="links">
+        <nuxt-link class="link" v-for="link in links" :to="link.to">
+          {{ link.title }}
+        </nuxt-link>
+      </nav>
+    </header>
+    <slot/>
   </div>
 </template>
+<script lang="ts" setup>
+const links = [
+  {
+    to: '/',
+    title: 'Components'
+  },
+  {
+    to: '/deps',
+    title: 'Dependencies'
+  },
+]
+
+</script>
 
 <style lang="scss" scoped>
 .default-layout {
   display: flex;
   flex-direction: column;
   padding: 30px 50px;
+
+  > .header {
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: #b1ffe6;
+    border-bottom: 2px solid #3cffbb;
+    padding: 15px 30px;
+
+    > .links {
+      display: flex;
+      grid-column-gap: 20px;
+      align-items: center;
+      list-style: none;
+      margin: unset;
+
+      > .link {
+        text-decoration: none;
+      }
+    }
+  }
 }
 </style>
