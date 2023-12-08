5c2cb5b94dca9a0bf730e2635d93268c75c0347e
Yan <yan.doshchinskiy@gmail.com>
2023-12-08 16:25:10 +0700

    feat: security headers updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
         enabled: isCSPEnabled,
         headers: {
             contentSecurityPolicy: {
-                'frame-src': '*.wildberries.ru/*'
+                'frame-src': '*.weather-panel.vercel.app/*'
             },
         },
     }
