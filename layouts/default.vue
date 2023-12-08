68d1290a00faa28bd2cc3a33144c01e87792b1b2
Yan <yan.doshchinskiy@gmail.com>
2023-12-08 15:32:31 +0700

    feat: security headers updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="default-layout">
     {{nodeEnv}}
+    {{nuxtNodeEnv}}
     <header class="header">
       <nav class="links">
         <nuxt-link class="link" v-for="link in links" :to="link.to">
@@ -24,6 +25,7 @@ const links = [
 ]
 
 const nodeEnv = process.env.NODE_ENV
+const nuxtNodeEnv = process.env.NUXT_NODE_ENV
 
 </script>
 
